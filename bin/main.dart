21157dae578305c979e52850a93dd26aafcdc5f1
Widyarso Joko Purnomo <widyjp10@gmail.com>
2020-04-30 11:23:19 +0700

    input output

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,17 @@
+import 'dart:io';
+
 void main() {
-  print('Anda memesan kopi');
-  print('Menggiling biji kopi');
-  print('Memanaskan air');
-  print('Mencampurkan air dengan kopi');
-  print('Menuangkan kopi ke dalam cangkir');
-  print('Kopi Anda telah siap!');
+  stdout.write('Berapa gram biji kopi yang ingin Anda masukkan : ');
+  int coffeeBeansStock = num.parse(stdin.readLineSync());
+  stdout.write('Berapa ml air yang ingin Anda masukkan : ');
+  int waterStock = num.parse(stdin.readLineSync());
+  stdout.write('Berapa ml susu yang ingin Anda masukkan : ');
+  int milkStock = num.parse(stdin.readLineSync());
+  stdout.write('Berapa banyak cup yang ingin Anda masukkan : ');
+  int cupStock = num.parse(stdin.readLineSync());
+
+  print('Mesin kopi Anda memiliki $coffeeBeansStock gram biji kopi');
+  print('Mesin kopi Anda memiliki $waterStock ml air');
+  print('Mesin kopi Anda memiliki $milkStock ml susu');
+  print('Mesin kopi Anda memiliki $cupStock cup');
 }
